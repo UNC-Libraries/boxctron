1c3d74d586266c5a180a9ea61a33c528539dbfd7
Erin Winter <wintere@cs.wisc.edu>
2023-06-16 15:17:06 -0400

    Initial commit, add new files, updated unit tests, fix typo in README. (#12)
    Co-authored-by: Erin Winter <wintere@ad.unc.edu>

--- src/tests/test_color_bar_data_module.py
+++ src/tests/test_color_bar_data_module.py
@@ -14,9 +14,9 @@ class TestColorBarDataModule:
       'base_image_path' : str(Path("fixtures/normalized_images").resolve()),
       'dataset_class' : 'src.datasets.color_bar_classifying_dataset.ColorBarClassifyingDataset',
       'max_dimension' : 1333,
-      'batch_size' : 2,
-      'test_percent': 0.2,
-      'val_percent': 0.2
+      'batch_size' : 3,
+      'test_percent': 0.25,
+      'val_percent': 0.25
     }, config_path)
     config = TrainingConfig(config_path)
 
@@ -25,32 +25,31 @@ class TestColorBarDataModule:
     train_dl = data_module.train_dataloader()
     
     train_list = list(iter(train_dl))
-    # Should be two batches in train set (3 items in batches of up to 2)
+    # Should be two batches in train set (2 batches of 3 items each)
     assert len(train_list) == 2
     train_batch1_images = train_list[0][0]
     train_batch1_labels = train_list[0][1]
-    assert list(train_batch1_images.shape) == [2, 3, 1333, 1333]
-    assert list(train_batch1_labels.shape) == [2]
+    assert list(train_batch1_images.shape) == [3, 3, 1333, 1333]
+    assert list(train_batch1_labels.shape) == [3]
     train_batch2_images = train_list[1][0]
     train_batch2_labels = train_list[1][1]
-    assert list(train_batch2_images.shape) == [1, 3, 1333, 1333]
-    assert list(train_batch2_labels.shape) == [1]
-    # assert len(train_batch2) == 1
+    assert list(train_batch2_images.shape) == [3, 3, 1333, 1333]
+    assert list(train_batch2_labels.shape) == [3]
     
     # dev should be in a single batch with one entry
     val_dl = data_module.val_dataloader()
     val_list = list(iter(val_dl))
-    assert len(val_list) == 1
+    assert len(val_list) == 1 # Number of batches
     val_batch1_images = val_list[0][0]
     val_batch1_labels = val_list[0][1]
-    assert list(val_batch1_images.shape) == [1, 3, 1333, 1333]
-    assert list(val_batch1_labels.shape) == [1]
+    assert list(val_batch1_images.shape) == [3, 3, 1333, 1333]
+    assert list(val_batch1_labels.shape) == [3]
 
     # test should be in a single batch with one entry
     test_dl = data_module.test_dataloader()
     test_list = list(iter(test_dl))
-    assert len(test_list) == 1
+    assert len(test_list) == 1 # Number of batches
     test_batch1_images = test_list[0][0]
     test_batch1_labels = test_list[0][1]
-    assert list(test_batch1_images.shape) == [1, 3, 1333, 1333]
-    assert list(test_batch1_labels.shape) == [1]
+    assert list(test_batch1_images.shape) == [3, 3, 1333, 1333]
+    assert list(test_batch1_labels.shape) == [3]
