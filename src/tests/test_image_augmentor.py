c254167ca6cbcb3d67bc67031c071d886ee9165e
Erin Winter <ewinter@unc.edu>
2023-07-18 09:19:00 -0400

    [BXC-4035] Add rounding for augmentation, no label test cases (#17)
    * finished coordinate recalculation
    
    * fix function arguments
    
    * adjust computations to use only measurements relative with width and height
    
    * add new unit tests, new rotation variable
    
    * remove logging calls, unused imports
    
    * fix edge case for images with no rectangle labels
    
    * fix edge case 2
    
    * add rounding, no label example + unit tests
    
    * Fix merge error
    
    * remove print statements

--- src/tests/test_image_augmentor.py
+++ src/tests/test_image_augmentor.py
@@ -122,15 +122,15 @@ class TestImageAugmentor:
       assert output_path.stem == 'P0004_0483_17486_rfv_s100'
       assert output_path.parent == tmp_path / 'output/ncc'
     aug_annos = from_json(config.annotations_output_path)
-    assert len(aug_annos) == 13 # 12 original + 1 augmented
-    assert aug_annos[12]['image'] == str(output_path)
-    assert aug_annos[12]['annotation_id'] == 8
+    assert len(aug_annos) == 14 # 13 original + 1 augmented
+    assert aug_annos[13]['image'] == str(output_path)
+    assert aug_annos[13]['annotation_id'] == 8
     assert sum(1 for x in config.output_base_path.rglob('*') if x.is_file()) == 1
     # Verify that augmented image was added to the file list output
     with open(config.file_list_output_path) as f:
       lines = f.read().splitlines()
       assert str(output_path) in lines
-      assert len(lines) == 13
+      assert len(lines) == 14
 
   def test_label_augmentation(self, config, tmp_path):
     # Seed guarantees correct selection
@@ -142,16 +142,31 @@ class TestImageAugmentor:
       assert (1276, 1333) == aug_img.size # Augmented image is rotated by 90
       assert output_path.stem == '00276_op0204_0001_r90_s75' 
     aug_annos = from_json(config.annotations_output_path)
-    assert len(aug_annos) == 13 # 12 original + 1 augmented
-    assert aug_annos[12]['image'] == str(output_path)
-    assert aug_annos[12]['rotation_type'] == 'r90'
-    assert len(aug_annos[12]['label']) == 2 #Subject, color_bar
-    assert aug_annos[12]['label'][0]['x'] == 0 
-    assert aug_annos[12]['label'][0]['y'] == 0
-    assert aug_annos[12]['label'][0]['width'] == 17.731629392971247
-    assert aug_annos[12]['label'][0]['height'] == 98.92966360856269
-    assert aug_annos[12]['label'][0]['original_width'] == 1276
-    assert aug_annos[12]['label'][0]['original_height'] == 1333
+    assert len(aug_annos) == 14 # 13 original + 1 augmented
+    assert aug_annos[13]['image'] == str(output_path)
+    assert aug_annos[13]['rotation_type'] == 'r90'
+    assert len(aug_annos[13]['label']) == 2 #Subject, color_bar
+    assert aug_annos[13]['label'][0]['x'] == 0 
+    assert aug_annos[13]['label'][0]['y'] == 0
+    assert aug_annos[13]['label'][0]['width'] == 17.7316
+    assert aug_annos[13]['label'][0]['height'] == 98.9297
+    assert aug_annos[13]['label'][0]['original_width'] == 1276
+    assert aug_annos[13]['label'][0]['original_height'] == 1333
+  
+  def test_augmentation_no_labels(self, config, tmp_path):
+    # Seed guarantees correct selection
+    random.seed(12)
+    subject = ImageAugmentor(config)
+    output_path = subject.process(Path('fixtures/normalized_images/gilmer/00276_op0217_0001_e.jpg'))
+    subject.persist_annotations()
+    with Image.open(output_path) as aug_img:
+      assert (1333, 924) == aug_img.size # Augmented image is rotated by 90
+      assert output_path.stem == '00276_op0217_0001_e_rfh_s100' 
+    aug_annos = from_json(config.annotations_output_path)
+    assert len(aug_annos) == 14 # 13 original + 1 augmented
+    assert aug_annos[13]['image'] == str(output_path)
+    assert aug_annos[13]['rotation_type'] == 'rfh'
+    assert aug_annos[13].get('label') == None
 
   def test_process_with_rotation_and_saturation(self, config, tmp_path):
     # Seed guarantees correct selection
@@ -164,12 +179,12 @@ class TestImageAugmentor:
       assert output_path.stem == 'P0004_0483_17486_r90_s75'
       assert output_path.parent == tmp_path / 'output/ncc'
     aug_annos = from_json(config.annotations_output_path)
-    assert len(aug_annos) == 13 # 12 original + 1 augmented
-    assert aug_annos[12]['image'] == str(output_path)
-    assert aug_annos[12]['annotation_id'] == 8
-    assert aug_annos[12]['rotation_type'] == 'r90'
-    assert aug_annos[12]['label'][0]['original_width'] == 1076 # Swap height and width from annotation
-    assert aug_annos[12]['label'][0]['original_height'] == 1333
+    assert len(aug_annos) == 14 # 13 original + 1 augmented
+    assert aug_annos[13]['image'] == str(output_path)
+    assert aug_annos[13]['annotation_id'] == 8
+    assert aug_annos[13]['rotation_type'] == 'r90'
+    assert aug_annos[13]['label'][0]['original_width'] == 1076 # Swap height and width from annotation
+    assert aug_annos[13]['label'][0]['original_height'] == 1333
     assert sum(1 for x in config.output_base_path.rglob('*') if x.is_file()) == 1
 
   def test_process_image_twice(self, config, tmp_path):
@@ -186,11 +201,11 @@ class TestImageAugmentor:
       assert output_path2.stem == 'P0004_0483_17486_r90fh_s100'
       assert output_path2.parent == tmp_path / 'output/ncc'
     aug_annos = from_json(config.annotations_output_path)
-    assert len(aug_annos) == 14 # 12 original + 2 augmented
-    assert aug_annos[12]['image'] == str(output_path)
-    assert aug_annos[12]['annotation_id'] == 8
-    assert aug_annos[12]['rotation_type'] == 'r90'
-    assert aug_annos[13]['image'] == str(output_path2)
+    assert len(aug_annos) == 15 # 13 original + 2 augmented
+    assert aug_annos[13]['image'] == str(output_path)
     assert aug_annos[13]['annotation_id'] == 8
-    assert aug_annos[13]['rotation_type'] == 'r90fh'
+    assert aug_annos[13]['rotation_type'] == 'r90'
+    assert aug_annos[14]['image'] == str(output_path2)
+    assert aug_annos[14]['annotation_id'] == 8
+    assert aug_annos[14]['rotation_type'] == 'r90fh'
     assert sum(1 for x in config.output_base_path.rglob('*') if x.is_file()) == 2
