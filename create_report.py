45f6e041bb12c9e17a951d024a9451c4eca56ff2
Rolando Rodriguez <50704607+RolRodr@users.noreply.github.com>
2023-08-04 16:22:27 -0400

    Update create_report.py
    updated script with new CLI flag to create agg data and new classes/methods from classifier_report.py

--- create_report.py
+++ create_report.py
@@ -6,8 +6,8 @@
 #   3) -x : (OPTIONAL) Substring that indicates the start of the normalized image path that will follow the normalized-url.
 #   4) -n : (OPTIONAL) HTTP url which the normalized image paths will be made relative to.
 #   5) -O : (OPTIONAL) Indicates whether the HTML report should be opened in default browser
-
-from src.utils.classifier_report import ReportGenerator
+#   6) -A : (OPTIONAL) Indicates whether aggregate data table will be created
+from src.utils.classifier_report import DataParser, ReportGenerator
 from pathlib import Path
 import argparse
 import os
@@ -23,6 +23,8 @@ parser.add_argument('-n', '--normalized-url', default=False, required=False,
                     help="URL which normalized image paths will be made relative to.")
 parser.add_argument('-O', '--open', action='store_true', required=False,
                     help="Flag indicates to open the html report in system's default browser.")
+parser.add_argument('-A', '--agg-data', action='store_true', required=False,
+                    help="Parser will provide additional table with aggregate data.")
 
 args = parser.parse_args()
 
@@ -35,18 +37,16 @@ assert os.path.splitext(args.file_path)[-1].lower() == '.csv'
 if args.output_path:
     assert os.path.splitext(args.output_path)[-1].lower() == '.html'
 
-
+parser = DataParser(args.file_path, args.normalized_url, args.substring)
 generator = ReportGenerator()
 
-generator.parse_csv(args.file_path)
-
-if args.normalized_url:
-    generator.normalize_urls(args.normalized_url, args.substring)
-    
-generator.create_html_page()
-
+data = parser.get_data()
+stats = False
+if args.agg_data:
+    stats = parser.get_stats()
+print(stats)
+generator.create_html_page(data, stats)
 generator.save_file(args.output_path)
-
 
 if args.open:
     generator.launch_page()
