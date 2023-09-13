93107d404328c210b37707ee40084dae5e493d0c
Erin Winter <ewinter@unc.edu>
2023-09-13 12:45:20 -0400

    Fix line endings, add helper function

--- src/datasets/color_bar_segmentation_dataset.py
+++ src/datasets/color_bar_segmentation_dataset.py
@@ -1,7 +1,7 @@
 from numpy import zeros, uint8
-from torch import max
 from src.datasets.color_bar_dataset import ColorBarDataset
 from src.utils.resnet_utils import load_for_resnet, load_mask_for_resnet
+from torchvision.transforms import ToPILImage
 from PIL import Image
 
 # Dataset with single class for labeling images as having a color bar or not
@@ -16,27 +16,33 @@ class ColorBarSegmentationDataset(ColorBarDataset):
     label_mask = label_mask.clamp(0,1)
     return image_data, label_mask
 
+  # Helper function for displaying tensors representing masks or images
+  def visualize_tensor(self, tensor):
+    transform = ToPILImage()
+    img = transform(tensor)
+    img.show()
+
   # Loads annotation data into self.labels in the same order they paths are listed in image_paths
   def load_labels(self, path_to_labels):
    # The label must be a mask rather than a binary [0,1] class 
-   for index, image_path in enumerate(self.image_paths):
-     if not image_path:
-       continue
-    # Add image dimensions
-     w, h = None, None
-     with Image.open(image_path) as img:
-      w, h = img.width, img.height
-     self.image_dimensions.append((w, h))
-     # Populate label masks
-     image_labels = path_to_labels[str(image_path)]
-     mask = zeros((h, w), dtype=uint8)
-     for label in image_labels:
-       if 'color_bar' in label['rectanglelabels']:
-        width, height = (label['width']), (label['height'])
-        x = int(label['x']/100 * label['original_width'])
-        y = int(label['y']/100 * label['original_height'])
-        x2 = x + int(width/100 * label['original_width']) # bar width
-        y2 = y + int(height/100 * label['original_height']) # bar height
-        mask[y:y2, x:x2] = 1 # Mark all pixels in the masked region with ones
-     self.labels.append(mask) 
+    for index, image_path in enumerate(self.image_paths):
+      if not image_path:
+        continue
+      # Add image dimensions
+      w, h = None, None
+      with Image.open(image_path) as img:
+        w, h = img.width, img.height
+      self.image_dimensions.append((w, h))
+      # Populate label masks
+      image_labels = path_to_labels[str(image_path)]
+      mask = zeros((h, w), dtype=uint8)
+      for label in image_labels:
+        if 'color_bar' in label['rectanglelabels']:
+          width, height = label['width'], label['height']
+          x = int(label['x']/100 * label['original_width'])
+          y = int(label['y']/100 * label['original_height'])
+          x2 = x + int(width/100 * label['original_width']) # bar width
+          y2 = y + int(height/100 * label['original_height']) # bar height
+          mask[y:y2, x:x2] = 1 # Mark all pixels in the masked region with ones
+      self.labels.append(mask) 
          
